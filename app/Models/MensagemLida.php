cb2fc2f129ae9c859284a5bb1a1a90eabd9089d8
Mauro Kruger <maurinho_k@hotmail.com>
2026-03-23 20:06:57 -0300

    cria relacionamentos para user sem necessidade de trait

--- app/Models/MensagemLida.php
+++ app/Models/MensagemLida.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Models\Traits\BelongsToMensagem;
 use App\Models\Traits\BelongsToUsuario;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MensagemLida extends Model
 {
-    use BelongsToMensagem, BelongsToUsuario;
+    use HasFactory, BelongsToMensagem, BelongsToUsuario;
 
     protected $table = 'mensagens_lidas';
 
